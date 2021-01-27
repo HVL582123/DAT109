3ddb19b0b954f7e7f335954d3811bd2cee8fcbde
HVL582123 <54101091+HVL582123@users.noreply.github.com>
2021-01-27 14:44:55 +0100

    Litt småpirk

--- Oblig1DAT109/src/dat109/hvl/no/Brett.java
+++ Oblig1DAT109/src/dat109/hvl/no/Brett.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Brett {
 	private final int STORRELSE = 100;
-	private final int ANTALL_RUNDER = 20;
+	private final int ANTALL_RUNDER = 10;
 	private int runderSpilt = 0;
 
 	private static Map<Integer, Integer> slange = new HashMap<>();
@@ -15,16 +15,15 @@ public class Brett {
 	Map<Spiller, Integer> spillerposisjon;
 
 	/**
-	 * Lager nytt brett med gitt liste av spillere. Plasserer dem så på rute 1
-	 * (Start)
+	 * Lager nytt brett med gitt liste av spillere. Plasserer dem så på rute 1 (Start)
+	 * Legger til stiger og slanger. Disse er forhåndsdefinert, ikke random. 
 	 */
 	public Brett(List<Spiller> spillere) {
 
 		this.spillerposisjon = new HashMap<Spiller, Integer>();
 
-		// Setter alle spillerene på rute 1
-		for (Spiller s : spillere) {
-			this.spillerposisjon.put(s, 1);
+		for (Spiller spiller : spillere) {
+			this.spillerposisjon.put(spiller, 1);
 		}
 
 		leggTilSlanger();
@@ -49,6 +48,7 @@ public class Brett {
 		} else {
 			if (posisjon == STORRELSE) { // Spilleren vinner
 				spillerposisjon.put(spiller, posisjon);
+				System.out.println(spiller.getNavn() + "vant!!");
 				ferdig = true;
 			}
 			if (null != slange.get(posisjon)) { // Spilleren treffer slange
@@ -76,42 +76,40 @@ public class Brett {
 		return ferdig;
 	}
 
-
 	private void leggTilStiger() {
 		stige.put(2, 38);
-//		stige.put(4, 14);
+		stige.put(4, 14);
 		stige.put(8, 31);
-//		stige.put(28, 84);
+		stige.put(28, 84);
 		stige.put(21, 42);
-//		stige.put(36, 44);
+		stige.put(36, 44);
 		stige.put(51, 67);
-//		stige.put(71, 91);
-		stige.put(80, 99);
+		stige.put(71, 91);
+		stige.put(80, 100);
 
 	}
-
 
 	private void leggTilSlanger() {
 
 		slange.put(98, 78);
-//		slange.put(95, 75);
-//		slange.put(92, 73);
+		slange.put(95, 75);
+		slange.put(92, 73);
 		slange.put(87, 24);
-//		slange.put(64, 60);
-//		slange.put(62, 18);
+		slange.put(64, 60);
+		slange.put(62, 18);
 		slange.put(56, 53);
-//		slange.put(47, 26);
-//		slange.put(49, 11);
+		slange.put(47, 26);
+		slange.put(49, 11);
 		slange.put(16, 5);
 	}
 
 	/**
 	 * Sjekker om vi har nådd det maksimale antall runder for brettet.
-	 * */
+	 */
 	public boolean maksRunder() {
 		boolean erFerdig = false;
-		runderSpilt++;
-		if (runderSpilt == ANTALL_RUNDER) {
+
+		if (this.runderSpilt == ANTALL_RUNDER) {
 			erFerdig = true;
 			System.out.println("Det er spilt " + runderSpilt + " runder!");
 			System.out.println("Maks antall runder nådd!");
@@ -121,29 +119,29 @@ public class Brett {
 
 		return erFerdig;
 	}
+
 	/**
-	 * Starter spillet med gitt liste av spillere, og med ternings-objekt. 
-	 * Det vil bli sjekket etter siste spillers kast om det er flere runder igjen å spille.
-	 * */
+	 * Starter spillet med gitt liste av spillere, og med ternings-objekt. Det vil
+	 * bli sjekket etter siste spillers kast om det er flere runder igjen å spille.
+	 */
 	public void spill(List<Spiller> spillere, Terning terning) {
-		int spillersTur = 0;
-		boolean ferdig = false;
+		boolean spillFerdig = false;
 		int spillerantall = spillere.size();
+		int spillersTur = 0;
 
-		while (!ferdig) {
+		while (!spillFerdig) {
 			Spiller denne = spillere.get(spillersTur);
-			int oyne = Terning.trill();
 
-			ferdig = flyttSpiller(denne, oyne);
-			if (ferdig) {
-				System.out.println(denne.getNavn() + " vant spillet!");
-			}
+			int oyne = Terning.trill();
+			spillFerdig = flyttSpiller(denne, oyne);
 
 			spillersTur++;
+			this.runderSpilt++;
+
 			if (spillersTur == spillerantall) {
 				spillersTur = 0;
 				if (maksRunder()) {
-					ferdig = true;
+					spillFerdig = true;
 				}
 			}
 		}
--- Oblig1DAT109/src/dat109/hvl/no/Stigespill.java
+++ Oblig1DAT109/src/dat109/hvl/no/Stigespill.java
@@ -7,27 +7,22 @@ import java.util.Scanner;
 public class Stigespill {
 
 	public static void main(String[] args) {
-		int antSpillere = 0;
-
 		Terning terning = new Terning();
-		Boolean ferdig = false;
+		int antSpillere = 1;
 		
 		Scanner scan = new Scanner(System.in);
 		
-		while (antSpillere <= 0 || antSpillere > 4) {
+		while (antSpillere <= 1 || antSpillere > 4) {
 			System.out.println("Skriv inn antall spillere, det kan være 2-4 stk");
 			antSpillere = scan.nextInt();
 		}
-			/**
-			 * Legger til spillere i en liste
-			 */
+		
 		List<Spiller> spillere = new ArrayList<Spiller>();
 		for (int i = 1; i <= antSpillere; i++){
 			Spiller spiller = new Spiller("Spiller " + i);
 			spillere.add(spiller);
 		}
 		
-		System.out.println(spillere.size());
 		Brett brett = new Brett(spillere);
 		brett.spill(spillere, terning);
 		
